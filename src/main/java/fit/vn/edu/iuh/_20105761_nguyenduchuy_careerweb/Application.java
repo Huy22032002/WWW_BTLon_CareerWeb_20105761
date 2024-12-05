d79264feac4b2a8f48d346a3ce0506b627323102
Huy22032002 <ndhuy2203@gmail.com>
2024-12-05 09:26:07 +0700

    'start-project'

---
+++ src/main/java/fit/vn/edu/iuh/_20105761_nguyenduchuy_careerweb/Application.java
@@ -0,0 +1,13 @@
+package fit.vn.edu.iuh._20105761_nguyenduchuy_careerweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/test/java/fit/vn/edu/iuh/_20105761_nguyenduchuy_careerweb/ApplicationTests.java
@@ -0,0 +1,13 @@
+package fit.vn.edu.iuh._20105761_nguyenduchuy_careerweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
